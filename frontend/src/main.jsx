9a35721dc8fdf4b6dfa03def1979c0a0ac96e9bc
Yogi <yogipanchal179@gmail.com>
2026-03-22 15:13:42 +0530

    fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 👈 add this
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>   {/* 👈 wrap your app */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
